b8a3d0be78ca2adf94743c27d5d2826f23ec3834
Lionel <lbanand@myluckyday.fr>
2022-02-04 12:33:22 +0100

    connect to sdk

--- demoappkotlin/src/main/java/com/activelook/demo/kotlin/MainActivity.kt
+++ demoappkotlin/src/main/java/com/activelook/demo/kotlin/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.activelook.demo.kotlin
 
+import android.Manifest
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.activelook.activelooksdk.DiscoveredGlasses
 import com.activelook.demo.kotlin.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import timber.log.Timber
@@ -20,32 +19,69 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private var viewModel : MainViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        manageButtonsListener()
+        managePermissions()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        observeSdkLogs()
+
+    }
+
+    private fun observeSdkLogs() {
+        viewModel?.scanResult?.observe(this) { glasse ->
+            Timber.e("scanResult Glasse : %s", glasse?.name ?: "no name")
+            viewModel?.connect()
+        }
+
+        viewModel?.errorMessage?.observe(this) {
+            Timber.e(it)
+            Snackbar.make(binding.fabConnect, "ERROR : $it", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
 
-        val vm = ViewModelProvider(this).get(MainViewModel::class.java)
-        vm.scanResult.observe(this) { glasse ->
-            Log.e("Glasse : ", glasse?.name ?: "no name")
-            vm.connect()
-        }
-        vm.errorMessage.observe(this,) {
+        viewModel?.message?.observe(this) {
             Timber.e(it)
+            Snackbar.make(binding.fabConnect, "message : $it" , Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
-        vm.scan()
+    }
+
+    private fun managePermissions() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WAKE_LOCK,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ), 0
+        )
+    }
+
+    private fun manageButtonsListener() {
+        binding.fabConnect.setOnClickListener { view ->
+            Snackbar.make(view, "Connect", Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show()
+            viewModel?.scan()
+        }
+
+        binding.fabDisconnect.setOnClickListener { view ->
+            Snackbar.make(view, "Disconnect", Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show()
+            viewModel?.stopScan ()
+            viewModel?.disconnect()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- demoappkotlin/src/main/java/com/activelook/demo/kotlin/MainViewModel.kt
+++ demoappkotlin/src/main/java/com/activelook/demo/kotlin/MainViewModel.kt
@@ -8,32 +8,61 @@ import androidx.lifecycle.viewModelScope
 import com.activelook.activelooksdk.DiscoveredGlasses
 import com.activelook.activelooksdk.Glasses
 import com.activelook.activelooksdk.Sdk
-import com.activelook.activelooksdk.types.FlowControlStatus
+import com.activelook.activelooksdk.types.GlassesUpdate
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     private var alSdk: Sdk? = null
-    //private val context = getApplication<Application>().applicationContext
 
     val scanResult : MutableLiveData<DiscoveredGlasses?> = MutableLiveData()
     private var discoveredGlasses : DiscoveredGlasses? = null
     private var connectedGlasses : Glasses? = null
 
-    val errorMessage : MutableLiveData<String?> = MutableLiveData() // voir pour mettre un state de connection
+    // voir pour mettre un state de connection
+    val errorMessage : MutableLiveData<String?> = MutableLiveData()
 
     val message : MutableLiveData<String?> = MutableLiveData()
 
     init {
-        alSdk = Sdk.init(getApplication<Application>().applicationContext)
+        viewModelScope.launch {
+            alSdk = Sdk.init(
+                getApplication<Application>().applicationContext,
+                "unused",
+                { gu: GlassesUpdate? ->
+                    Timber.e("onUpdateStart ", String.format(" %s", gu))
+                },
+                { gu: GlassesUpdate? ->
+                    Timber.e("onUpdateProgress: %s", String.format(" %s", gu))
+                },
+                { gu: GlassesUpdate? ->
+                    Timber.e("onUpdateSuccess : %s", String.format(" %s", gu))
+                }
+            ) { gu: GlassesUpdate? ->
+                Timber.e("onUpdateError   : %s", String.format(" %s", gu))
+            }
+        }
+    }
+
+    fun stopScan() {
+        viewModelScope.launch {
+            alSdk?.stopScan()
+            scanResult.postValue(null)
+            discoveredGlasses = null
+        }
+    }
+
+    fun disconnect() {
+        connectedGlasses?.disconnect()
     }
 
     fun scan() {
         viewModelScope.launch {
             alSdk?.startScan(Consumer { glasses ->
+                alSdk?.stopScan()
                 scanResult.postValue(glasses)
                 discoveredGlasses = glasses
-                alSdk?.stopScan()
             })
         }
     }
@@ -42,11 +71,14 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         discoveredGlasses?.connect(
             { glasses ->
                 //onconnected
-                message.postValue("Success")
+                message.postValue("Connected Success")
 
-                 connectedGlasses.setOnDisconnected { glasses ->
+                connectedGlasses = glasses
+                 connectedGlasses?.setOnDisconnected { glasses ->
+                     message.postValue("OnDisconnect")
                      glasses.disconnect()
                  }
+
                 connectedGlasses?.subscribeToFlowControlNotifications { status ->
                     message.postValue(String.format("Flow control: %s", status.name))
                 }
